dc5480d0201924010079b3c9f6ea661bac87f053
nykh <nicholas.ykhuang@gmail.com>
2016-11-28 11:43:16 -0800

    Debugged update function, now it produces good result

--- adm_lena.m
+++ adm_lena.m
@@ -1,50 +1,46 @@
 load('lena_101.mat');
 
 img = im2double( img_padded );
-
 fimg = abs( fft2( img ) );
-
-
-
 
 figure(1001);
 imshow(img);
 
-figure(1002);
-imagesc( log10( fftshift(fimg.^2) ) );
-axis image;
-colormap(jet);
-colorbar;
+% figure(1002);
+% imagesc( log10( fftshift(fimg.^2) ) );
+% axis image;
+% colormap(jet);
+% colorbar;
 
-rng(5566);
+%% Initialization
+
+rng(841);
 
 x_init = support_constraint( rand(size(img)), mask );
 y_init = yconstraint( rand(size(img)), fimg );
 
-figure(1011);
-imshow(x_init);
+% figure(1011);
+% imshow(x_init);
+% 
+% figure(1012);
+% imshow(y_init);
 
-figure(1012);
-imshow(y_init);
+%% ADM
 
-n_iter = 500;
+n_iter = 150;
 
 x = x_init;
 y = y_init;
 lambda = zeros(size(x));
-pi0 = zeros(size(x));
 
 beta0 = 0.9;
-nu = beta0;
 
 for t = 1:n_iter
     disp(['iteration #' int2str(t)]);
     % update x
     x = support_constraint( y - lambda, mask );
-    % update pi0
-
     % update y
-    y = yconstraint( x + pi0, fimg );
+    y = yconstraint( x + lambda, fimg );
     % update lambda
     lambda = lambda + beta0 * (x-y);
 end
@@ -56,14 +52,14 @@ axis image;
 colormap(gray);
 colorbar;
 
-figure(2002);
-imagesc( y );
-axis image;
-colormap(gray);
-colorbar;
-
-figure(2003);
-imagesc( lambda );
-colormap(jet);
-axis image;
-colorbar;
+% figure(2002);
+% imagesc( y );
+% axis image;
+% colormap(gray);
+% colorbar;
+% 
+% figure(2003);
+% imagesc( lambda );
+% colormap(jet);
+% axis image;
+% colorbar;
